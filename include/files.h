84cc603bae6c664d3f3a4d7f4f3b5af9e303ff0c
JASL22 <alexandersjhonn@gmail.com>
2026-05-26 07:54:44 -0500

    mejoras

--- include/files.h
+++ include/files.h
@@ -1,7 +1,5 @@
 #ifndef FILES_H
 #define FILES_H
-
 #include <stddef.h>
 void serve_file(int client_fd, const char *uri, const char *www_root);
-
-#endif /* FILES_H */
+#endif 
--- include/mime.h
+++ include/mime.h
@@ -1,18 +1,4 @@
 #ifndef MIME_H
 #define MIME_H
-
-/*
- * get_mime_type: dado un nombre de archivo (o su extensión),
- * retorna el tipo MIME correspondiente como string.
- *
- * Si la extensión no se reconoce, retorna "application/octet-stream"
- * (descarga binaria genérica), que es el comportamiento estándar seguro.
- *
- * Ejemplos:
- *   get_mime_type("index.html") → "text/html"
- *   get_mime_type("logo.png")   → "image/png"
- *   get_mime_type("data.xyz")   → "application/octet-stream"
- */
 const char *get_mime_type(const char *filename);
-
-#endif /* MIME_H */
+#endif 
